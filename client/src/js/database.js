814091ceda4dc4e80003c9708037ddb98dff9094
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-08-29 16:25:24 -0400

    comments for js files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,33 +1,41 @@
 import { openDB } from "idb";
 
-const initdb = async () =>
-  openDB("jate", 1, {
+// Initialize the database
+const initdb = async () => {
+  await openDB("jate", 1, {
+    // Add database schema if not already initialized
     upgrade(db) {
       if (db.objectStoreNames.contains("jate")) {
         console.log("jate database already exists");
         return;
       }
+      // Create an object store with auto-incrementing key
       db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
       console.log("jate database created");
     },
   });
+};
 
+// Function to save data to the database
 export const putDb = async (content) => {
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ jate: content });
+  const request = store.put({ value: content });
   const result = await request;
-  console.log("Data saved to the database", result);
+  console.log("🚀 - data saved to the database", result);
 };
 
+// Function to retrieve data from the database
 export const getDb = async () => {
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
   const request = store.getAll();
   const result = await request;
-  console.log(result);
+  console.log("🚀 - data read from database", result);
+  return result;
 };
 
+// Initialize the database schema
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,28 +1,23 @@
-const buttonInstall = document.getElementById("buttonInstall");
-let deferredPrompt;
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
 window.addEventListener("beforeinstallprompt", (event) => {
-  deferredPrompt = event;
-  buttonInstall.classList.remove("hidden");
+  // Store the triggered event for later use
+  window.deferredPrompt = event;
+  // Show the installation button by removing the hidden class
+  butInstall.classList.toggle("hidden", false);
 });
+butInstall.addEventListener("click", async () => {
+  // Retrieve the stored prompt event
+  const promptEvent = window.deferredPrompt;
 
-buttonInstall.addEventListener("click", async () => {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    const choiceResult = await deferredPrompt.userChoice;
-
-    if (choiceResult.outcome === "accepted") {
-      console.log("User accepted the PWA installation prompt.");
-    } else {
-      console.log("User dismissed the PWA installation prompt.");
-    }
-
-    deferredPrompt = null;
-    buttonInstall.classList.add("hidden");
+  if (!promptEvent) {
+    return;
   }
+  promptEvent.prompt();
+  // Reset the deferred prompt variable as it can only be used once
+  window.deferredPrompt = null;
+  butInstall.classList.toggle("hidden", true);
 });
-
 window.addEventListener("appinstalled", (event) => {
-  deferredPrompt = null;
+  window.deferredPrompt = null;
 });
