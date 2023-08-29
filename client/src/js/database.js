96b62a029631abf06975ead9946eb3db040fd2c2
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-08-29 19:26:17 -0400

    database change

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,41 +1,30 @@
 import { openDB } from "idb";
 
-// Initialize the database
-const initdb = async () => {
-  await openDB("jate", 1, {
-    // Add database schema if not already initialized
+const initdb = async () =>
+  openDB("jate", 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
         return;
       }
-      // Create an object store with auto-incrementing key
       db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
     },
   });
-};
 
-// Function to save data to the database
 export const putDb = async (content) => {
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ value: content });
-  const result = await request;
-  console.log("🚀 - data saved to the database", result);
+  const request = store.put({ id: 1, value: content });
+  await request;
 };
 
-// Function to retrieve data from the database
 export const getDb = async () => {
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
   const request = store.getAll();
   const result = await request;
-  console.log("🚀 - data read from database", result);
-  return result;
+  return result.value;
 };
 
-// Initialize the database schema
 initdb();
